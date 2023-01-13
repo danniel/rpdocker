9fa3211b2e81ded702c87ea4f69a4c4c02cfdad1
Daniel Ursache Dogariu <contact@danniel.net>
2023-01-13 02:52:35 +0200

    Fix static path and remove message compilation action

--- docker/s6-rc.d/init/init.sh
+++ docker/s6-rc.d/init/init.sh
@@ -10,14 +10,14 @@ if [ "${RUN_MIGRATION}" = "yes" ]; then
     python3 manage.py migrate --run-syncdb
 fi
 
-if [ "${RUN_COMPILE_MESSAGES}" = "yes" ]; then
-    echo "Compiling translation messages"
-    python3 manage.py compilemessages
-fi
+# if [ "${RUN_COMPILE_MESSAGES}" = "yes" ]; then
+#     echo "Compiling translation messages"
+#     python3 manage.py compilemessages
+# fi
 
 if [ "${RUN_COLLECT_STATIC}" = "yes" ]; then
     echo "Collect static"
-    mkdir -p /var/www/rapidpro/static
+    mkdir -p /var/www/rapidpro/sitestatic
     python3 manage.py collectstatic --noinput
 fi
 
